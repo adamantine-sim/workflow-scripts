3e24580ce80afe543598bb822c406fb17481bb60
stvdwtt <dewittsj@ornl.gov>
2025-05-15 13:16:42 -0400

    added 17-4 PH objective function and scanpath writere

---
+++ 17_4_PH_analysis/17_4_PH_analysis.py
@@ -0,0 +1,176 @@
+import numpy as np
+import pyvista
+import os
+import glob
+import matplotlib.pyplot as plt
+import sys
+from scipy.spatial import cKDTree
+import argparse
+
+
+# ----------------------------------------------------------
+# Function to get the simulation iteration count (should be generalized across workflow-scripts)
+# ----------------------------------------------------------
+
+def get_iteration_count(path_prefix):
+    filename_pattern = path_prefix + '*.pvtu'
+    files = glob.glob(filename_pattern)
+
+    iteration_numbers = []
+    for f in files:
+        temp = f.replace('.pvtu', '')
+        post_id_index = temp.rfind('.')
+        iteration_string = temp[post_id_index+1:]
+        iteration_count = int(iteration_string)
+        iteration_numbers.append(iteration_count)
+
+    iteration_numbers.sort()
+
+    return iteration_numbers
+
+def check_indexing(directory, adamantine_filename, iteration_numbers):
+    # This function is not currently used since the indexing is not consistent
+    print("Checking indexing...")
+    
+    filename = directory + adamantine_filename + '.' + str(iteration_numbers[0]) + '.pvtu'
+    mesh_0 = pyvista.read(filename)
+
+    consistent_indexing = False
+
+    for iteration_number in iteration_numbers:
+        filename = directory + adamantine_filename + '.' + str(iteration_number) + '.pvtu'
+        mesh_i = pyvista.read(filename)
+
+        consistent_indexing = mesh_0.number_of_points == mesh_i.number_of_points and np.allclose(mesh_0.points, mesh_i.points)
+
+        if not consistent_indexing:
+            print("ERROR: Indexing is not consistent between time steps! Iteration number: ", iteration_number)
+            sys.exit()
+        
+    return consistent_indexing
+
+def get_time_series(directory, adamantine_filename, field_name, line_plots):
+    # NOTE: About 1 out of every 10 points has one or more spurious drops to zero after being activated. I'm not sure what
+    # that is caused by. Maybe this is due to points that lie on the activated/deactivated interface?
+
+    print("Getting the time series...")
+
+    iteration_numbers = get_iteration_count(directory + adamantine_filename)
+
+    # Load first dataset (reference mesh)
+    dataset_0 = pyvista.read(f"{directory}{adamantine_filename}.{iteration_numbers[0]}.pvtu")
+    n_points = dataset_0.number_of_points
+
+    # Allocate the time series array
+    val = np.zeros((len(iteration_numbers), n_points))
+
+    ref_points = dataset_0.points
+
+    for idx, iteration_number in enumerate(iteration_numbers):
+        file_to_plot = f"{directory}{adamantine_filename}.{iteration_number}.pvtu"
+        dataset = pyvista.read(file_to_plot)
+
+        # Build KDTree of current timestep points
+        tree = cKDTree(dataset.points)
+
+        # Match reference points to nearest in current dataset
+        distances, nearest_indices = tree.query(ref_points)
+
+        if np.sum(distances) > 0.01:
+            print(f'WARNING: timestep {iteration_number} has large total match distance: {np.sum(distances):.4f}')
+
+        # Vectorized temperature lookup
+        val[idx] = dataset.point_data[field_name][nearest_indices]
+
+    # Optional plotting for to see the single-point temperature history
+    if (line_plots):
+        for i in range(min(5, val.shape[1])):  # plot first 5 points
+            plt.figure()
+            plt.plot(iteration_numbers, val[:, i], 'b.-')
+            plt.title(f"Point {i} Temperature Over Time")
+            plt.xlabel("Iteration")
+            plt.ylabel("Temperature")
+            plt.grid(True)
+            plt.show()
+
+    return val
+
+
+def objective_17_4_PH(time_series):
+    # NOTE: I have a special check in here to ignore cases where the temperature dips down below 5K to deal with artifacts
+    # in the data. I'm still not sure why those occur.
+
+    T_solidus = 1713.0 # Solidus temperature for 17-4PH
+    T_Ms = 150.0 + 273.15 # Martensite start temperature in K
+    T_ppt = 480.0 + 273.15 # Optimal temperature for precipitation
+    T_band_ppt = 50.0 # Width of the band around T_ppt that "count" for the objective function
+
+    per_point_scores = np.zeros(time_series.shape[1])
+    
+    # Step through time
+    for n in range(0, time_series.shape[1]):
+        single_point_series = time_series[:,n]
+
+        has_melted = False
+        has_cooled_to_Ms = False
+
+        for temperature in single_point_series:
+            if temperature >= T_solidus:
+                    has_melted = True
+                    per_point_scores[n] = 0
+            elif has_melted and (not has_cooled_to_Ms):
+                if temperature > 5.0 and temperature < T_Ms:
+                    has_cooled_to_Ms = True
+            elif has_cooled_to_Ms:
+                if np.abs(temperature-T_ppt) < T_band_ppt/2.0:
+                    per_point_scores[n] = per_point_scores[n] + 1
+
+    return per_point_scores
+
+
+def plot_score_on_mesh(directory, adamantine_filename, per_point_scores):
+    iteration_numbers = get_iteration_count(directory + adamantine_filename)
+    filename = directory + adamantine_filename + '.' + str(iteration_numbers[0]) + '.pvtu'
+    dataset = pyvista.read(filename)
+
+    dataset.point_data['score'] = per_point_scores
+    pl = pyvista.Plotter()
+    dataset.set_active_scalars("score")
+    clipped = dataset.clip('y')
+    pl.add_mesh(clipped, show_edges=True)
+    pl.show()
+
+
+def analysis(directory, adamantine_filename, field_name, line_plots, volume_plot):
+    time_series = get_time_series(directory, adamantine_filename, field_name, line_plots)
+
+    per_point_scores = objective_17_4_PH(time_series)
+
+    if volume_plot:
+        plot_score_on_mesh(directory, adamantine_filename, per_point_scores)
+
+    # I may also want to promote low variance between the score at different locations
+    score = np.sum(per_point_scores)
+
+    print("Score:", score)
+
+    return score
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Analysis routine for 17-4PH",
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-d", "--data-directory", help="location of the VTK files to load")
+    parser.add_argument("-f", "--base-filename", help="base filename for the VTK files")
+    parser.add_argument("-v", "--variable", default="temperature", help='variable name in the VTK files')
+    parser.add_argument("--line-plots", default=False, help='whether to plot some single-point temperature histories')
+    parser.add_argument("--volume-plot", default=False, help='whether to plot a 3D cross-section of the per-point score')
+    args = parser.parse_args()
+
+    directory = args.data_directory #'/Users/71d/Documents/workspace/DockerWorkspace_adamantine/power_rook_2x/'
+    adamantine_filename = args.base_filename #'solution_dwell'
+    field_name = args.variable #"temperature"
+    line_plots = args.line_plots
+    volume_plot = args.volume_plot
+
+    analysis(directory, adamantine_filename, field_name, line_plots, volume_plot)

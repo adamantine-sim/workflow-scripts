c605b19ed3ad89a91ff36cf6ce16b098f10d1885
stvdwtt <dewittsj@ornl.gov>
2025-09-19 01:57:08 -0400

    some minor performance optimizations for field_variable_comparison

--- field_variable_comparison/field_variable_comparison.py
+++ field_variable_comparison/field_variable_comparison.py
@@ -47,18 +47,16 @@ def merged_field_variable_comparison(dataset1, dataset2, field_name):
     tree = KDTree(dataset2_cell_centers)
     distances, indices = tree.query(dataset1_cell_centers)
 
-    total_diff = 0.0
+    # Get all cell point ids at once
+    cell1_point_ids = [dataset1.get_cell(i).point_ids for i in range(len(indices))]
+    cell2_point_ids = [dataset2.get_cell(indices[i]).point_ids for i in range(len(indices))]
 
-    for i in range(0, len(indices)):
-        cell1 = dataset1.get_cell(i)
-        cell2 = dataset2.get_cell(indices[i])
+    # Flatten and stack for vectorized access
+    vals1 = np.concatenate([dataset1.point_data[field_name][ids] for ids in cell1_point_ids])
+    vals2 = np.concatenate([dataset2.point_data[field_name][ids] for ids in cell2_point_ids])
 
-        vals1 = dataset1.point_data[field_name][cell1.point_ids]
-        vals2 = dataset2.point_data[field_name][cell2.point_ids]
-
-        diff = np.abs(vals1-vals2)
-
-        total_diff = total_diff + diff.sum()
+    diff = np.abs(vals1 - vals2)
+    total_diff = diff.sum()
     
     return total_diff
 
@@ -66,14 +64,16 @@ def pvtu_field_variable_comparison(file1, file2, field_name):
 
     full_filename1 = file1 + ".pvtu"
     dataset1 = pyvista.read(full_filename1)
+    dataset1 = dataset1.clean(tolerance=1e-6)
     dataset1.set_active_scalars(field_name)
 
     full_filename2 = file2 + ".pvtu"
     dataset2 = pyvista.read(full_filename2)
+    dataset2 = dataset2.clean(tolerance=1e-6)
     dataset2.set_active_scalars(field_name)
 
-    pl = pyvista.Plotter(off_screen=True)
-    pl.add_mesh(dataset1, show_edges=True, cmap='plasma')
+    #pl = pyvista.Plotter(off_screen=True)
+    #pl.add_mesh(dataset1, show_edges=True, cmap='plasma')
     #pl.show()
     #pl.save_graphic("field1_out.pdf")
 
